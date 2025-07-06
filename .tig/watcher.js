37131bdf1ce7f3fb2f6fdb60c3c961682137f18d
prashant-m4 <prashantsingh090798@gmail.com>
2025-07-06 17:26:50 +0530

    feat: added file path splitter

--- .tig/watcher.js
+++ .tig/watcher.js
@@ -1,15 +1,26 @@
 import { exec } from 'child_process';
 
-const cmd = 'find .. -path \'../.git\' -prune -o -type f -print';
+function separateAllFilePaths(stdout) {
+  const filePaths = stdout.trim().split('\n');
+  console.log(filePaths); 
+}
 
-exec(cmd, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`Stderr: ${stderr}`);
-    return;
-  }
-  console.log(`Output:\n${stdout}`);
-});
+function listFilesUnderWatch() {
+  const cmd = 'find .. -path \'../.git\' -prune -o -type f -print';
+
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Stderr: ${stderr}`);
+      return;
+    }
+    separateAllFilePaths(stdout);
+  });
+}
+
+listFilesUnderWatch();
+// verfiy if it the same folder as earlier to prevent iisues caused during copy paste of tig folder from one repo to another.
+
