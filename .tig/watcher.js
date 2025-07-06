ed63988c636fcbf577d38432fb5dc0dda1cded22
prashant-m4 <prashantsingh090798@gmail.com>
2025-07-06 18:26:58 +0530

    feat: added file hasher

--- .tig/watcher.js
+++ .tig/watcher.js
@@ -1,26 +1,39 @@
 import { exec } from 'child_process';
+import { readFileSync } from 'fs';
+import { createHash } from 'crypto';
+
+function hashFiles(filePaths) {
+  let hashedFiles = [];
+  for (const filePath of filePaths) {
+    const fileContent = readFileSync(filePath, 'utf8');
+    const hashedFilePath = createHash('MD5').update(filePath).digest('hex');
+    const hashedContent = createHash('sha256').update(fileContent).digest('hex');
+    const hashedFile = hashedFilePath + "." + hashedContent;
+    hashedFiles.push(hashedFile);
+  }
+  return hashedFiles;
+}
 
 function separateAllFilePaths(stdout) {
   const filePaths = stdout.trim().split('\n');
-  console.log(filePaths); 
+  return filePaths;
 }
 
 function listFilesUnderWatch() {
-  const cmd = 'find .. -path \'../.git\' -prune -o -type f -print';
-
+  const cmd = "find .. \\( -path '../.git' -o -path '../.tig' \\) -prune -o -type f -print";
+  
   exec(cmd, (error, stdout, stderr) => {
-    if (error) {
+    if (error || stderr) {
       console.error(`Error: ${error.message}`);
       return;
     }
-    if (stderr) {
-      console.error(`Stderr: ${stderr}`);
-      return;
-    }
-    separateAllFilePaths(stdout);
+    const filePaths = separateAllFilePaths(stdout);
+    const hashedFiles = hashFiles(filePaths);
+    console.log(hashedFiles);
   });
 }
 
 listFilesUnderWatch();
 // verfiy if it the same folder as earlier to prevent iisues caused during copy paste of tig folder from one repo to another.
+// add tigignore
 
