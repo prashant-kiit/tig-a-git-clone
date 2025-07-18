83a71c76119a7a509309503b0a1da2a532fae86a
prashant-m4 <prashantsingh090798@gmail.com>
2025-07-18 15:04:58 +0530

    feat: added data validation for registration

--- .tig/register.js
+++ .tig/register.js
@@ -1,17 +1,53 @@
 import db from './tigstore.js';
 import { collection, getDocs, addDoc, query, where } from 'firebase/firestore';
 import promptSync from 'prompt-sync';
+import { z } from 'zod';
 
 const prompt = promptSync({ sigint: true });
+
+const MAX = 3;
+
+const emailSchema = z.email();
+
+const passwordSchema = z.string()
+    .min(8, 'Password must be at least 8 characters long')
+    .max(100, 'Password too long')
+    .regex(/[a-z]/, 'Must contain at least one lowercase letter')
+    .regex(/[A-Z]/, 'Must contain at least one uppercase letter')
+    .regex(/[0-9]/, 'Must contain at least one digit')
+    .regex(/[^a-zA-Z0-9]/, 'Must contain at least one special character');
+
+const schema = {
+    emailId: emailSchema,
+    password: passwordSchema
+}
+
+function readInput(inputType) {
+    let input = "";
+    let count = 0;
+    while (count < MAX) {
+        input = prompt(`Enter your ${inputType}: `);
+        const result = schema[inputType].safeParse(input);
+        if (result.success) {
+            return input;
+        }
+        if (count === MAX - 1) {
+            console.log(`Error: Invalid ${inputType} format`);
+            process.exit(0);
+        }
+        console.log(`Error: Invalid ${inputType} format. Please try again.`);
+        count++;
+    }
+}
 
 async function addUser(user) {
     try {
         const usersRef = collection(db, 'users');
-        const q = query(usersRef, where('userId', '==', user.userId));
+        const q = query(usersRef, where('emailId', '==', user.emailId));
         const snapshot = await getDocs(q);
 
         if (!snapshot.empty) {
-            console.log('User ID already exists. Please choose a unique userId.');
+            console.log('User ID already exists. Please choose a unique emailId.');
             process.exit(1);
         }
 
@@ -24,11 +60,13 @@ async function addUser(user) {
     }
 }
 
-function runRegister(userId) {
-    const password = prompt('Enter your password: ');
+// email Id and password validation
+function runRegister() {
+    const emailId = readInput("emailId")
+    const password = readInput("password")
 
     const user = {
-        userId,
+        emailId,
         password,
         isLoggedIn: false
     }
--- .tig/tig.js
+++ .tig/tig.js
@@ -6,14 +6,14 @@ import runRegister from "./register.js"
 const args = process.argv.splice(2);
 if (args[0] === "stage") runStaging(args[1]);
 
-else if (args[0] === "commit") runCommit(args[1]);
+else if (args[0] === "commit" && args.length === 2) runCommit(args[1]);
 
-else if (args[0] === "push") runPush();
+else if (args[0] === "push" && args.length === 1) runPush();
 
-else if (args[0] === "register") runRegister(args[1]);
+else if (args[0] === "register" && args.length === 1) runRegister();
 
 else {
-    console.log("Not a valid cmd:", args[0])
+    console.log("Not a valid cmd: tig", args.join(' '))
 }
 
 
