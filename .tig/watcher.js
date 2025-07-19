1ed466c3ac005cd30b3f217e251727dc134609fa
prashant-m4 <prashantsingh090798@gmail.com>
2025-07-19 19:31:21 +0530

    feat: added auto index.json creater

--- .tig/watcher.js
+++ .tig/watcher.js
@@ -1,7 +1,26 @@
 import { exec } from 'child_process';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { readFileSync, existsSync, writeFileSync } from 'fs';
+import { resolve, join } from 'path';
 import { deserializeFromIndex, serializeToIndex, hashFilePath, hashFileContent } from './helper.js'
+
+const indexFileName = 'index.json'
+
+function ensureIndexJsonExists() {
+  const filePath = join(process.cwd(), indexFileName);
+  const initialContent = JSON.stringify({
+    pushed: [],
+    committed: [],
+    staged: {},
+    watched: {}
+  }, null, 4);
+
+  if (!existsSync(filePath)) {
+    writeFileSync(filePath, initialContent, 'utf-8');
+    console.log('index.json created.');
+  } else {
+    console.log('index.json already exists.');
+  }
+}
 
 function processAllFilePaths(stdout) {
   const filePaths = stdout.trim().split('\n');
@@ -42,13 +61,14 @@ function storeHashedFiles(hashedFiles) {
 }
 
 function listFilesUnderWatch() {
-  const cmd = "find .. \\( -path '../.git' -o -path '../.tig' -o -path '../.tigserver'\\) -prune -o -type f -print";
+  const cmd = `find .. \\( -path '../.git' -o -path '../.tig' -o -path '../tigserver' \\) -prune -o -type f -print`;
 
   exec(cmd, (error, stdout, stderr) => {
     if (error || stderr) {
       console.error(`Error: ${error.message}`);
       return;
     }
+    ensureIndexJsonExists();
     const filePaths = processAllFilePaths(stdout);
     const hashedFiles = hashFiles(filePaths);
     storeHashedFiles(hashedFiles);
